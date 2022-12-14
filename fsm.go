349ead15fb9246bafc45c957b826912833889014
Marco Filippone <m.filip@hotmail.it>
2022-12-14 07:14:33 +0100

    fsm v2

---
+++ fsm.go
@@ -0,0 +1,71 @@
+package main
+
+import "log"
+
+type StateType int
+
+const (
+	Start StateType = iota
+	Other
+	Terminal
+)
+
+type State struct {
+	id        string
+	stateType StateType
+}
+
+type Context map[string]string
+type Action func(Context) Context
+
+type Event struct {
+	name   string
+	action Action
+}
+
+type Rule func(ctx Context) bool
+
+type Transition struct {
+	from  *State
+	to    *State
+	rule  Rule
+	event Event
+}
+
+type Fsm struct {
+	states      []State
+	transitions []Transition
+}
+
+type Runner struct {
+	fsm          Fsm
+	context      Context
+	currentState *State
+}
+
+func (r *Runner) Next() bool {
+	if r.currentState.stateType == Terminal {
+		return false
+	}
+	// Find relevant transition
+	var transition Transition
+	for _, t := range r.fsm.transitions {
+		if t.from == r.currentState && t.rule(r.context) {
+			transition = t
+		}
+	}
+	log.Printf("Firing event %s from state %s", transition.event.name, r.currentState.id)
+	r.context = transition.event.action(r.context)
+	r.currentState = transition.to
+	return true
+}
+
+func (r *Runner) Run() {
+	var ok bool
+	for {
+		ok = r.Next()
+		if !ok {
+			break
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,35 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"log"
-
-	"github.com/calcifer777/go-fsm/fsm"
 )
 
+func AlwaysFire(ctx Context) bool { return true }
+
+func NoOp(ctx Context) Context { return ctx }
+
 func main() {
-	node := fsm.NewPassNode("start", "step1", false)
-	// fmt.Println(node)
-	fmt.Println("ID: ", node.ID())
-	node.Run(map[string]string{})
 
-	node2 := fsm.NewPassNode("step1", "end", false)
-	node3 := fsm.NewPassNode("end", "", true)
-	nodes := make([]fsm.Node, 0)
-	nodes = append(nodes, node, node2, node3)
-	fmt.Println(nodes)
+	noOp := Event{name: "no-op", action: NoOp}
 
-	myFsm := fsm.NewFsm(nodes)
-	fmt.Println(myFsm)
-
-	runner := fsm.NewFsmRunner(*myFsm, "start")
-	fmt.Println(runner)
-
-	out, err := runner.Run(nil)
-	if err != nil {
-		log.Fatal(err)
+	states := []State{
+		{id: "start", stateType: Start},
+		{id: "step1", stateType: Other},
+		{id: "end", stateType: Terminal},
 	}
-	fmt.Println("output context", out)
-	fmt.Println(runner)
+
+	transitions := []Transition{
+		{from: &states[0], to: &states[1], rule: AlwaysFire, event: noOp},
+		{from: &states[1], to: &states[2], rule: AlwaysFire, event: noOp},
+	}
+
+	fsm := Fsm{states: states, transitions: transitions}
+
+	runner := Runner{
+		fsm:          fsm,
+		context:      nil,
+		currentState: &states[0],
+	}
+
+	log.Printf("FSM starting state: %s", runner.currentState.id)
+	runner.Run()
+	log.Printf("FSM ending state: %s", runner.currentState.id)
 
 }
